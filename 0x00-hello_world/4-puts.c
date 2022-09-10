e950be0d236f403b1ff6d4ae91f96f5d90e8e1ac
Glory <oyenekanglory99@gmail.com>
2022-09-10 11:04:13 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,11 +5,10 @@
  *
  *Description: using the main function
  *this program prints "Programming is like building a multilingual puzzle
- *Return : 0
+ *Return: always 0
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle \n");
-  return (0);
-  
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
