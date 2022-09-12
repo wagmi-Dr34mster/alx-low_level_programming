0d5e81a9b00a479044ba89f932ca9f124b585d78
Glory <oyenekanglory99@gmail.com>
2022-09-12 01:24:12 -0700

    more tasks

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- *outcome - it prints string with printf function
+ *main - it prints string with printf function
  *
  *Description: using the outcome
  *this program prints the string with proper grammar,
  *but the outcome is a piece of art
  *Return: always 0
  */
-int outcome(void)
+int main(void)
 {
 printf("with proper grammar, but the outcome is piece of art,");
 return (0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *main - print the various data type sizes on -x64 computer
+ *
+ *Description: the main function prints sizes for char, int
+ *long int, long long int and float data types in -x64 computer
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf(Size of a char : 1 byte(s)
+	 Size of an int : 4 byte(s)
+	 Size of a long int : 8 byte(s)
+	 Size of a long long int : 8 byte(s)
+	 Size of a float : 4 byte(s));
+Return(0);
+}
