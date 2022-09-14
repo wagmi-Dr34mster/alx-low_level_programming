d04192f1223fd0f3872211b9d134830952ea71f4
Glory <oyenekanglory99@gmail.com>
2022-09-13 20:37:45 -0700

    d

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,14 @@
 #include "main.h"
 
 
-
 /**
-
  * main - print putchar
-
  * Return: always 0
-
  */
-
-
 
 int main(void)
 
 {
-
 	_putchar('_');
 
 	_putchar('p');
@@ -35,9 +28,5 @@ int main(void)
 	_putchar('\n');
 
 
-
 	return (0);
-
-
-
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,5 @@
 #ifndef HOLBERTON_H_INCLUDED
-
 #define HOLBERTON_H_INCLUDED
-
-
 
 int _putchar(char);
 
@@ -31,7 +28,6 @@ void print_to_98(int n);
 void print_times_table(int n);
 
 void num(int r, int n);
-
 
 
 #endif
