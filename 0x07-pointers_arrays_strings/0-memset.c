a72408a0a5668f0c25c1d4a6ecf827e9c3e18d63
Glory <oyenekanglory99@gmail.com>
2022-09-26 16:40:20 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,13 +6,15 @@
  * @b: char to be used
  * @n: number of bytes
  *
- *Return: s
+ *Return: pointer to memory area
  */
 
 char *_memset(char *s, char b, unsigned int n);
 {
-int i;
-for (i = 0; i < n; i++)
-s[i] = b;
+while (n)
+{
+s[n - 1] = b;
+n--
+}
 return (s)
 }
