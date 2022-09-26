286607f01aa0808ee7d600038a1d8d52a14c06c5
Glory <oyenekanglory99@gmail.com>
2022-09-26 16:53:58 -0700

    see

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * int prototypes()
+ * void prototypes()
+ */
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
