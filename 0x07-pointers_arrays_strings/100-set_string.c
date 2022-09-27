a8cb58d00dd647b1ccfe69bb1db4a56632485b98
Glory <oyenekanglory99@gmail.com>
2022-09-26 19:52:09 -0700

    str

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set the value of a pointer to a char
+ * @s: pointer to change
+ * @to: string to change pointer
+ *
+ * Return: void
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strstr - loctes a substring
+ * @haystack: string in which to check for needle
+ * @needle: substring to find in haystack
+ *
+ * Return: pointer to beginning of needle in haystack or NULL if no match
+ */
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int = 0, j = 0;
+while (haystack[i])
+{
+while (needle[j] && (haystack[i] == needle[0]))
+{
+if (haystack[i + j] == needle[j])
+j++;
+else
+break;
+}
+if (needle[j])
+{
+i++;
+j = 0;
+}
+else
+return (haystack + 1);
+}
+return(0);
+}
