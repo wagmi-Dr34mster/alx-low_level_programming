8eda04b9e15c64d8fd990b625db4dcf6129123f3
Glory <oyenekanglory99@gmail.com>
2022-09-30 07:16:02 -0700

    static library

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset - function to fill memory area
+ * @s: pointer to memory area
+ * @b: char to be used
+ * @n: number of bytes
+ *
+ *Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+while (n)
+{
+s[n - 1] = b;
+n--;
+}
+return (s);
+}
